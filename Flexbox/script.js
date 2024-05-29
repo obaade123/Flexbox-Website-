be7f53dbe952936bfb769a93679c4ac623e7171c
obaade123 <104175185+obaade123@users.noreply.github.com>
2024-05-30 00:22:48 +0100

    Add files via upload

---
+++ Flexbox/script.js
@@ -0,0 +1,8 @@
+document.querySelector(".nav__btn").addEventListener("click", showNav)
+
+function showNav() {
+  document.querySelector(".nav__links").classList.toggle("show__links")
+}
+
+var name = "john"
+console.log(name)
